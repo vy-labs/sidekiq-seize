721a59a8d9abbee076f81a4bbcc0365f321637ef
rajat <gyl.rajat@gmail.com>
2019-04-09 17:54:15 +0530

    chain

--- lib/sidekiq-seize.rb
+++ lib/sidekiq-seize.rb
@@ -1,8 +1,6 @@
 require 'sidekiq'
 require 'sidekiq/middleware/server/seize'
 
-::Sidekiq.configure_server do |config|
-  config.server_middleware do |chain|
-    chain.add Sidekiq::Middleware::Server::Seize
-  end
+::Sidekiq.server_middleware do |chain|
+  chain.add Sidekiq::Middleware::Server::Seize
 end
